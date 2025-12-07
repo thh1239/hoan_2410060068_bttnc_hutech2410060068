acad65c19794f97b66a3c8586f651947c04ca5c4
hoan.2410060068 <thh1239@gmail.com>
2025-12-07 17:48:17 +0700

    update_lab

--- baitap1/1.6.1.py
+++
@@ -1,3 +0,0 @@
-print("Hello, World!")
-print("My name is Hoan")
-print("Huetech University")
--- baitap1/1.6.2.3.py
+++
@@ -1,5 +0,0 @@
-so = int(input("Nhập một số nguyên: "))
-if so % 2 == 0:
-    print(f"Số {so} là số chẵn.")
-else:
-    print(f"Số {so} không phải là số chẳn.")
